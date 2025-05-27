6f97773982577fa58f5ef48d64edfdebce2da7a6
gayatri mahajan <gayatrijori1999@gmail.com>
2025-05-27 15:39:02 +0530

    Creating new file as constants and add the attributes having the same pattern in that file to optomize code

--- src/screens/Register/index.jsx
+++ src/screens/Register/index.jsx
@@ -1,5 +1,11 @@
 import { useForm } from "react-hook-form";
 import FormInputField from "../../components/FormInputField";
+import {
+  formName,
+  formPlaceholder,
+  formType,
+  regValidators,
+} from "../../utils/uiConstants";
 
 const Register = () => {
   const {
@@ -28,9 +34,9 @@ const Register = () => {
 
         {/* **************** Username */}
         <FormInputField
-          type={"text"}
-          placeholder={"Username"}
-          name={"username"}
+          type={formType.emailType}
+          placeholder={formPlaceholder.username}
+          name={formName.username}
           inputHandler={{
             ...register("username", { required: "Username is required" }),
           }}
@@ -39,14 +45,14 @@ const Register = () => {
 
         {/* **************** Email */}
         <FormInputField
-          type={"text"}
-          placeholder={"you@gmail.com"}
-          name={"email"}
+          type={formType.emailType}
+          placeholder={formPlaceholder.email}
+          name={formName.emailName}
           inputHandler={{
             ...register("email", {
               required: "Email is required",
               pattern: {
-                value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+                value: regValidators.emailValidator,
                 message: "Invalid email address",
               },
             }),
@@ -56,14 +62,14 @@ const Register = () => {
 
         {/* **************** Phone number */}
         <FormInputField
-          type={"tel"}
-          placeholder={"Phone Number"}
-          name={"phone"}
+          type={formType.phoneType}
+          placeholder={formPlaceholder.phone}
+          name={formName.phone}
           inputHandler={{
             ...register("phone", {
               required: "Phone number is required",
               pattern: {
-                value: /^[0-9]{10}$/,
+                value: regValidators.phone,
                 message: "Phone number must be 10 digits",
               },
             }),
@@ -73,15 +79,15 @@ const Register = () => {
 
         {/* **************** Password */}
         <FormInputField
-          type={"password"}
-          placeholder={"********"}
-          name={"password"}
+          type={formType.passType}
+          placeholder={formPlaceholder.password}
+          name={formName.passName}
           inputHandler={{
             ...register("password", {
               required: "Password is required",
-              minLength: {
-                value: 6,
-                message: "Password should be at least 6 characters",
+              pattern: {
+                value: regValidators.passValidator,
+                message: "Incorrect password",
               },
             }),
           }}
@@ -89,10 +95,10 @@ const Register = () => {
         />
 
         {/* **************** Confirm Password */}
-         <FormInputField
-          type={"password"}
-          placeholder={"Confirm Password"}
-          name={"confirmPassword"}
+        <FormInputField
+          type={formType.passType}
+          placeholder={formPlaceholder.confirmPass}
+          name={formName.confirmPass}
           inputHandler={{
             ...register("confirmPassword", {
               required: "Please confirm your password",
