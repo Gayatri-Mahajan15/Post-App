e64f8abc343d427e0594b00b87aadddcf1093c52
gayatri mahajan <gayatrijori1999@gmail.com>
2025-06-04 13:30:54 +0530

    Adding facebook login for login and register form with the new branch Gayatri/facebook-login

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,15 @@
 import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
 import React from "react";
+import ReactFacebookLogin from "react-facebook-login";
 
 const Footer = () => {
-
-     const handleGoogleLogin = (response) => {
+  const handleGoogleLogin = (response) => {
     console.log("Google login success:", response);
     // Send response.credential to backend
+  };
+
+  const responseFacebook = (response) => {
+    console.log("Facebook response:", response);
   };
 
   return (
@@ -17,15 +21,24 @@ const Footer = () => {
       </div>
 
       <div className="flex justify-center gap-4 mt-4">
-       
         <GoogleOAuthProvider clientId="142194786397-e40000jsupi3ruqnpb82iv4j9egbb7md.apps.googleusercontent.com">
-        <GoogleLogin
-          onSuccess={handleGoogleLogin}
-          onError={() => console.log("Google login failed")}
-          useOneTap
-        />
-      </GoogleOAuthProvider>
+          <GoogleLogin
+            onSuccess={handleGoogleLogin}
+            onError={() => console.log("Google login failed")}
+            useOneTap
+          />
+        </GoogleOAuthProvider>
       </div>
+
+      <div className="flex justify-center gap-4 mt-4">
+         <ReactFacebookLogin
+          appId="9897627100331926"
+          autoLoad={true}
+          fields="name,email,picture"
+          callback={responseFacebook}
+        />
+      </div>
+
     </div>
   );
 };
